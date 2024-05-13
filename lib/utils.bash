bf19b7ce113e4338ce8094b11e9298812a973550
Rakuyo <rakuyo.mo@gmail.com>
2024-05-13 10:47:38 +0800

    feat: Try to use git command to implement `list-all` command

--- lib/utils.bash
+++ lib/utils.bash
@@ -10,26 +10,15 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
-
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+sort_versions() {
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+  		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
 
 list_all_versions() {
-	RELEASES_URL="https://api.github.com/repos/${SOURCES_REPO_NAME}/releases"
-
-	curl_cmd="curl -s"
-	if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-		curl_cmd="$curl_cmd -H \"Authorization: token $GITHUB_API_TOKEN\""
-	fi
-
-	# Fetch list of releases and get names of each release tag
-	eval "$curl_cmd $RELEASES_URL" |
-		grep tag_name |
-		sed 's/"tag_name": //g;s/"//g;s/,//g' |
-		(tac 2>/dev/null || tail -r) |
-		xargs
+	git ls-remote --tags --refs "https://github.com/${SOURCES_REPO_NAME}" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+	  	sed 's/^v//'
 }
 
 download_style_file() {
